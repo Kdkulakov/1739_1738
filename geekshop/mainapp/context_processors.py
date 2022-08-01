030116c4460a7f7087952e72caa7f1bdf5ce86ee
kulakov <kdkulakov@gmail.com>
2022-08-01 22:34:54 +0300

    поменял проект

--- geekshop/mainapp/context_processors.py
+++ geekshop/mainapp/context_processors.py
@@ -1,14 +1,13 @@
-def menu_links(request):
-    print(f'context processor basket works')
-    return {
-        "menu_links": [
-            {"url": "main", "active": ["main"], "name": "домой"},
-            {
-                "url": "products:all",
-                "active": ["products:all", "products:category"],
-                "name": "продукты",
-            },
-            {"url": "contact", "active": ["contact"], "name": "контакты"},
-        ]
-    }
+from basketapp.models import Basket
 
+
+def basket(request):
+    print(f'context processor basket works')
+    basket = []
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    return {
+        'basket': basket,
+    }
