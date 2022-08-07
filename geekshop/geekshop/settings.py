420e176983b3412de18c31c909b013c9f3e70337
KosarevDV <KosarevDV@users.noreply.github.com>
2022-08-07 22:28:58 +0300

    Косарев ДВ, гр. GU_python_1738 Приступил к ДЗ №1

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -147,17 +147,32 @@ AUTH_USER_MODEL = "authapp.ShopUser"
 LOGIN_URL = "auth:login"
 
 
-# Mail settings
-DOMAIN_NAME = "http://localhost:8000"
+# # Mail settings
+# DOMAIN_NAME = "http://localhost:8000"
+#
+# # For work mail
+# # EMAIL_HOST = "localhost"
+# # EMAIL_PORT = "2025"
+# # EMAIL_HOST_USER = None
+# # EMAIL_HOST_PASSWORD = None
+# # EMAIL_USE_SSL = False
+#
+#
+# # For test
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = "tmp/email-messages/"
 
-# For work mail
-# EMAIL_HOST = "localhost"
-# EMAIL_PORT = "2025"
-# EMAIL_HOST_USER = None
-# EMAIL_HOST_PASSWORD = None
-# EMAIL_USE_SSL = False
 
+DOMAIN_NAME = 'http://localhost:8000'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+EMAIL_HOST_USER = 'django@geekshop.local'
+EMAIL_HOST_PASSWORD = 'geekshop'
+EMAIL_USE_SSL = False
 
-# For test
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = "tmp/email-messages/"
+#вариант python -m smtpd -n -c DebuggingServer localhost:25
+EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+
+#вариант логирования сообщений почты в виде файлов вместо отправки
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'tmp/email-messages/'
